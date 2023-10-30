9ba469598dd32a1d8ab4887861c1ff65b185043a
onionmccabbage <onionmccabbage@live.com>
2023-10-30 10:48:31 +0000

    using property

--- class_a.py
+++ class_a.py
@@ -6,15 +6,20 @@ class Point:
     x will be a number, y will also be a number"""
     # every method that belongs to a class MUST take 'self' as an argument
     def __init__(self, new_x, new_y): # this is the initializer for our class
-        self.x = new_x # we are setting the property of the isntance to the incoming value
-        self.y = new_y
+        # in classes we typically use 'name mangling' __x
+        self.__x = new_x # we are setting the property of the isntance to the incoming value
+        self.__y = new_y
     # here are some more methods of this class (a method is simple a function that is inside a class)
-    def set_x(self, new_x):
+    def set_x(self, new_x): # setter or mutator
         '''check to make sure new_x is a number'''
         if (new_x.isnumeric()): # careful isnumeric only checks string values  # type(new_x)==float
-            self.x = float(new_x) # cast teh string to a floating point value
+            self.__x = float(new_x) # cast teh string to a floating point value
         else:
             raise TypeError() # or we could set a default
+    def get_x(self): # getter or accessor
+        return self.__x
+    # we can make these get/set methods behave like properties
+    x = property(get_x, set_x)
 
 # __xxx__ is known as dunder
 if __name__ == '__main__':
@@ -25,6 +30,6 @@ if __name__ == '__main__':
     p2 = Point(True, False) # every time we make an isntance, the __init__ is called
     # we call methods of a class like this (note the brackets)
     p2.set_x('99')
-    print(p2.x) # 99.0
-    print(p1.x, p1.y) # dot notation
+    print(p2.__x) # 99.0
+    print(p1.__x, p1.y) # dot notation
     # print(p2[x], p2['y']) # square-bracket notation - fix this...
