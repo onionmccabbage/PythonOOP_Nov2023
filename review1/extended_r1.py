0b133c69fb4ef96593843be0af1602fb9e23bdad
onionmccabbage <onionmccabbage@live.com>
2023-10-30 14:39:50 +0000

    Sample solution for Review 1

---
+++ review1/extended_r1.py
@@ -0,0 +1,19 @@
+from review_class import NumberFun
+from datetime import datetime
+
+class ExtendedNumberFun(NumberFun):
+    def __init__(self, num, flag):
+        super().__init__(num)
+        self.flag = flag # could use get/set methods
+    def __str__(self):
+        txt = super().__str__()
+        if self.flag == True:
+            d = self.__dict__
+            return txt + f'\nClass dictionary contains {d}'
+        else:
+            return txt
+    
+
+if __name__ == '__main__':
+    r = ExtendedNumberFun(100, True)
+    print(r)
