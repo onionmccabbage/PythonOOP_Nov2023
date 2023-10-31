eed4ce77d8b26d221268184cb8f965884265e377
onionmccabbage <onionmccabbage@live.com>
2023-10-31 12:29:21 +0000

    using requests

--- using_remote_data/api_access.py
+++ using_remote_data/api_access.py
@@ -8,8 +8,11 @@ import requests
 # we can use requests in immediate code, in functions or in classes
 def getUsers():
     '''We will retreive user s data from a web-based API'''
-    url = 'https://jsonplaceholder.typicode.com/users'
+    cat = 'todos'
+    num = 42
+    url = f'http://jsonplaceholder.typicode.com/{cat}/{num}'
     response = requests.get(url)
+    # response.text or response.html or response.xml ....
     return response.json() # convert the JSON data into a Python structure
 
 if __name__ == '__main__':
