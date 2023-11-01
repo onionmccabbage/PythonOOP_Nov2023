701131f1984d6e93b8f666281f3b8d59355326d7
onionmccabbage <onionmccabbage@live.com>
2023-11-01 12:24:57 +0000

    usig sys argv with class server/client

--- microservices/class_client.py
+++ microservices/class_client.py
@@ -1,13 +1,21 @@
 import socket
+import sys
 
 class MicroClient():
+    def __init__(self):
+        '''if there are system argumetn variables, use them
+        otherwise send a default message'''
+        if len(sys.argv)>1: 
+            self.msg = ' '.join(sys.argv[1:]) # combine the system args into a string
+        else:
+            self.msg = 'default'
     def MyClient(self):
         '''make requests to our microservice'''
         cli = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # must match server
         config_t = ('localhost', 9874) # IP and port
         cli.connect(config_t)
         # send a message ot the server
-        msg = 'quit'
+        msg = self.msg
         cli.send(msg.encode()) # we are obliged to encode anything passed over http
         # we can choose to handle any respoinse from the server
         response = cli.recv(1024)
